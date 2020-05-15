909939dc543bad2e5d2895c50bdbeb5a73d76dfd
Rafael Monteiro <radiorafael@hotmail.com>
2020-05-14 23:57:42 -0400

    Update everysome.js

--- everysome.js
+++ everysome.js
@@ -3,12 +3,21 @@ var goodUsers = [
     {id: 2 },
     {id: 3 }
 ]
+testAllValid = [
+    { id: 2 },
+    { id: 1 }
+  ]
+
+
+
+
 
 
 function checkUsersValid(goodUsers) {
-    return function allUsersValid(submittedUsers) {
-      
+    return function allUsersValid(testAllValid) {
+        return goodUsers.some(function(testAllValid) {
+            return goodUsers === testAllValid;
+          });
     };
-  }
-  
+}
   module.exports = checkUsersValid
